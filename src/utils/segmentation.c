9b29ba06c375b0eee6ba42b3032dec8cd766ecc6
Gauthier Denglos <gauthier.denglos@r02p04.vj303.sm.cri.epita.net>
2018-10-21 15:51:42 +0200

    Last push

--- src/utils/segmentation.c
+++ src/utils/segmentation.c
@@ -4,11 +4,12 @@ void parcours_vertical(SDL_Surface *image, int startline, int endline);
 
 void remplissage(SDL_Surface *image, int startline, int endline, int startcolum, int endcolum);
 
+//horizontal course of the image
 void parcours_horizontal(SDL_Surface *image)
 {
-	int oneblack = 0 ; // bool to know if one star char
-	int startline = 0;
-	int endline;
+	int oneblack = 0 ; // bool to know if we met a black pixel 
+	int startline = 0; // First line to be met 
+	int endline; //last line to be met 
 
   for(int y = 0; y < image->h; y++)
   {
@@ -43,11 +44,12 @@ void parcours_horizontal(SDL_Surface *image)
   }
 }
 
+//vertical course of the image
 void parcours_vertical(SDL_Surface *image, int startline, int endline)
 {
-  int oneblack = 0;
-  int startcolum = 0;
-  int endcolum;
+  int oneblack = 0; 
+  int startcolum = 0; // first colum to be met  
+  int endcolum; // last line to be met 
   for(int x = 0; x < image->w; x++)
   {
     int oneblank = 1;
@@ -80,6 +82,7 @@ void parcours_vertical(SDL_Surface *image, int startline, int endline)
   }
 }
 
+//Change the color of the pixel which are in the square
 void remplissage(SDL_Surface *image, int startline, int endline, int startcolum, int endcolum)
 {
   for (int y = startline; y <= endline; y++)
@@ -88,7 +91,6 @@ void remplissage(SDL_Surface *image, int startline, int endline, int startcolum,
     {
       Uint32 pixel = get_pixel(image, x, y);
       Uint8 r = 255, g = 0, b = 0;
-      //SDL_GetRGB(pixel, image->format, &r, &g, &b);
       pixel = SDL_MapRGB(image->format, r, g, b);
       put_pixel(image, x, y, pixel);
     }
